a8c4b26b4ed9aaff6c40811d311e7a154bc536cf
Yuko <n.nikita.6607@gmail.com>
2021-01-23 15:32:26 +0900

    test commit

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\HTML;
 
 // 追記
+//test
+//test
 use App\News;
 
 class NewsController extends Controller
@@ -13,13 +15,13 @@ class NewsController extends Controller
     public function index(Request $request)
     {
         $posts = News::all()->sortByDesc('updated_at');
-        
+
         if (count($posts) > 0) {
             $headline = $posts->shift();
         } else {
             $headline = null;
         }
-        
+
         // news/index.blade.php ファイルを渡している
         // またViewテンプレートにheadline, posts, という変数を渡している
         return view('news.index', ['headline' => $headline, 'posts' => $posts]);
